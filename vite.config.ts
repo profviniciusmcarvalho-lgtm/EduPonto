690422efada35a1e7083f3106bd6ed61eb48fcc3
profviniciusmcarvalho-lgtm <profviniciusmcarvalho@gmail.com>
2026-04-01 09:05:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+export default {
+  base: '/EduPonto/'
+}
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
